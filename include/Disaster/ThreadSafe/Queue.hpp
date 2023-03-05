1252bf55d86ee30b9f7a41442b03ae70fde53f39
pyxiion <me@pyxiion.ru>
2023-03-05 17:57:50 +0300

    Async queue

---
+++ include/Disaster/ThreadSafe/Queue.hpp
@@ -0,0 +1,51 @@
+#ifndef PX_DISASTER_THREAD_SAFE_QUEUE_HPP
+#define PX_DISASTER_THREAD_SAFE_QUEUE_HPP
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+
+namespace px::disaster::thread_safe {
+  template<class T>
+  class Queue {
+  private:
+    mutable std::mutex m_mutex;
+    std::queue<T> m_queue;
+    std::condition_variable m_dataCond;
+
+  public:
+    Queue() {}
+    Queue(const Queue &other) {
+      std::lock_guard<std::mutex> lk(other.m_mutex);
+      m_queue = other.m_queue;
+    }
+
+    void Push(T value) {
+      std::lock_guard<std::mutex> lk(m_mutex);
+      m_queue.push(value);
+      m_dataCond.notify_one();
+    }
+
+    void WaitAndPop(T &value) {
+      std::unique_lock<std::mutex> lk(m_mutex);
+      m_dataCond.wait(lk, [this]{return !m_queue.empty();});
+      value = m_queue.front();
+      m_queue.pop();
+    }
+
+    bool TryPop(T &value) {
+      std::lock_guard<std::mutex> lk(m_mutex);
+      if (m_queue.empty())
+        return false;
+      value = m_queue.front();
+      m_queue.pop();
+      return true;
+    }
+
+    bool Empty() const {
+      std::lock_guard<std::mutex> lk(m_mutex);
+      return m_queue.empty();
+    }
+  };
+}
+
+#endif // PX_DISASTER_THREAD_SAFE_QUEUE_HPP
