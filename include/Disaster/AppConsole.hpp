065318d1c512bb3d6e10e2a18282d767538fd3b9
pyxiion <me@pyxiion.ru>
2023-03-05 17:55:35 +0300

    Swap public and private. Add comments

--- include/Disaster/AppConsole.hpp
+++ include/Disaster/AppConsole.hpp
@@ -6,7 +6,7 @@
 #include <utility>
 
 #include <Disaster/Script/ScriptEngine.hpp>
-#include <imgui-sfml/imgui.h>
+#include <imgui/imgui.h>
 
 namespace px::disaster {
 
@@ -16,6 +16,46 @@ namespace px::disaster {
 
   class AppConsole {
     friend class gameplay::Game;
+  public:
+    AppConsole();
+
+    /// @brief Formats the message and prints it to the console and cout.
+    void AddLog(const char *fmt, ...) IM_FMTARGS(2);
+
+    /// @brief Clears the console.
+    void ClearLog();
+
+    /// @brief Prints a message from the string.
+    /// @param message message
+    void Log(const std::string &message);
+
+    /// @brief Prints a string in "[warning] message" format. It will be highlighted in yellow in the console.
+    /// @param message message
+    void Warn(const std::string &message);
+
+    /// @brief Prints a string in "[error] message" format. It will be highlighted in red in the console.
+    /// @param message message
+    void Error(const std::string &message);
+    
+    /// @brief Executes the console command.
+    /// @param commandLine command line
+    void Execute(const std::string &commandLine);
+
+    /// @brief Draws the console itself in the UI using ImGui
+    void Draw();
+
+    // Allows get the value from the eval
+    void _Grab(int v);
+    void _Grab(unsigned int v);
+    void _Grab(bool v);
+    void _Grab(float v);
+    void _Grab(double v);
+    void _Grab(const std::string &v);
+    void _Grab();
+
+    /// @brief Returns an instance of the last console already created or nullptr.
+    static AppConsole *GetInstance();
+
   private:
     char m_inputBuf[256];
     std::vector<std::string> m_items;
@@ -33,44 +73,27 @@ namespace px::disaster {
 
     asIScriptEngine *m_scriptEngine;
 
+    /// @brief configures AngelScript engine
     void ConfigureEngine();
 
+    /// @brief Executes AngelScript code
+    /// @param arg code
     void Exec(std::string &arg);
+
+    /// @brief Executes AngelScript expresion and prints the value
+    /// @param arg expression
     void Eval(std::string &arg);
+
     void AddVariable(std::string &arg);
     void DeleteVariable(std::string &arg);
     void AddFunction(std::string &arg);
     void DeleteFunction(std::string &arg);
     void ListVariables();
 
+    /// @brief Manages console input history and auto-complete commands on Tab
     int TextEditCallback(ImGuiInputTextCallbackData *data);
 
     static AppConsole *instance;
-
-  public:
-    AppConsole();
-
-    void AddLog(const char *fmt, ...) IM_FMTARGS(2);
-    void ClearLog();
-
-    void Log(const std::string &message);
-    void Warn(const std::string &message);
-    void Error(const std::string &message);
-    
-    void Execute(const std::string &commandLine);
-
-    void Draw();
-
-    // Get eval value
-    void _Grab(int v);
-    void _Grab(unsigned int v);
-    void _Grab(bool v);
-    void _Grab(float v);
-    void _Grab(double v);
-    void _Grab(const std::string &v);
-    void _Grab();
-
-    static AppConsole *GetInstance();
   };
 }
 
