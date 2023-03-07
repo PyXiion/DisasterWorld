0098d915168b62e8b34889efe5787d3f8b3fedc8
pyxiion <me@pyxiion.ru>
2023-03-07 18:18:47 +0300

    Concluded the code in try catch in the release

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,16 @@
 int main(int argc, char** argv) {
   EASY_PROFILER_ENABLE;
   EASY_MAIN_THREAD;
-  // try {
+
+#ifdef NDEBUG
+  try {
     px::disaster::Program::GetInstance().Main();
-  // } catch (const std::exception &e) {
-    // std::cout << "An error has occurred: " << e.what() << std::endl;
-  // }
+  } catch (const std::exception &e) {
+    std::cout << "An error has occurred: " << e.what() << std::endl;
+  }
+#else
+  px::disaster::Program::GetInstance().Main();
+#endif
   profiler::dumpBlocksToFile("debug.prof");
   return 0;
 }
