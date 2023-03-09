12f117f3c85041d47f4e063fc907cfaa31806773
pyxiion <me@pyxiion.ru>
2023-03-09 15:21:43 +0300

    Removed unnecessary constructor in Color

--- src/System/Color.cpp
+++ src/System/Color.cpp
@@ -1,7 +1,6 @@
 #include <Disaster/System/Color.hpp>
 
 namespace px::disaster {
-  Color::Color() : r(0), g(0), b(0), a(0) {}
   Color::Color(ColorComponent r, ColorComponent g, ColorComponent b, ColorComponent a)
     : r(r), g(g), b(b), a(a) {}
 }
